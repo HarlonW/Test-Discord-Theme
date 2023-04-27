477d145246dbecaaa18cd55508bee668b0c234d2
HarlonW <132016289+HarlonW@users.noreply.github.com>
2023-04-27 18:51:35 -0400

    Update Neptune.theme.cs

--- Neptune.theme.cs
+++ Neptune.theme.cs
@@ -14,7 +14,7 @@
 
 :root {
   --neptune-main-bg: #302f2f;
-  --neptune-main-color: #cdaef3;
+  --neptune-main-color: #741BE2;
   --neptune-sec-color: #bb86fc;
   --neptune-home-icon: url(https://cdn.discordapp.com/attachments/922287059991953468/1101264938154066050/LHoroeU.png);
   --watermark-filter-invert: 0%;
